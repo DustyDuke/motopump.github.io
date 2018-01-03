22bf1a335a168bd3cfbffb8290bb5e0d3991155c
Lena <34030418+DustyDuke@users.noreply.github.com>
2018-01-03 18:50:23 +0300

    form
    contact php

---
+++ contact-form.php
@@ -0,0 +1,41 @@
+﻿<?php
+
+/* Задаем переменные */
+$name = htmlspecialchars($_POST["name"]);
+$surname = htmlspecialchars($_POST["surname"]);
+$tel = htmlspecialchars($_POST["tel"]);
+$email = htmlspecialchars($_POST["email"]);
+$message = htmlspecialchars($_POST["message"]);
+$bezspama = htmlspecialchars($_POST["bezspama"]);
+
+/* Ваш адрес и тема сообщения */
+$address = "kotenevaelena@rambler.ru";
+$sub = "заявка на тренеровку";
+
+/* Формат письма */
+$mes = "Сообщение с сайта ХХХ.\n
+Имя отправителя: $name 
+Фамилия: $surname
+Телефон отправителя: $tel
+Почта отправителя: $email
+Текст сообщения:
+$message";
+
+
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from = "Reply-To: $email \r\n";
+if (mail($address, $sub, $mes, $from)) {
+	header('Refresh: 5; URL=http://biznessystem.ru');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо отправлено, через 5 секунд вы вернетесь на страницу XXX</body>';}
+else {
+	header('Refresh: 5; URL=http://biznessystem.ru');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо не отправлено, через 5 секунд вы вернетесь на страницу YYY</body>';}
+}
+exit; /* Выход без сообщения, если поле bezspama чем-то заполнено */
+?>
